78bf038a1751aaa2fc09673cd4e25ab773af2619
Diego Oliveira de Souza Alves <diego.alves@solinftec.com.br>
2024-06-20 17:03:55 -0300

    package test

---
+++ Tests/lib-navigation-iosTests/lib_navigation_iosTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import lib_navigation_ios
+
+final class lib_navigation_iosTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
