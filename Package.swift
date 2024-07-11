34e441d2e80ee911d8e251add72660757d426078
Diego Oliveira de Souza Alves <diego.alves@solinftec.com.br>
2024-07-11 17:45:48 -0300

    Update test

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
             name: "lib-navigation-ios",
             targets: ["lib-navigation-ios"]),
     ],
-//    dependencies: [
-//        .package(url: "https://github.com/maplibre/maplibre-navigation-ios.git", from: "3.0.0")
-//    ],
+    dependencies: [
+        .package(url: "https://github.com/maplibre/maplibre-navigation-ios.git", from: "3.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
